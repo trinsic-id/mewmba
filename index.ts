3690a22875651dd1a933e9cbc14ef529f5c80475
Scott Phillips <polygonguru@gmail.com>
2022-02-18 18:32:19 -0500

    mewmba refactor and make rickroll work

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import {Mewmba} from "./mewmba";
 import {Player} from "@gathertown/gather-game-common";
 import {randomInt} from "crypto";
 import {RandomColor} from "./neonLights";
+import {GatherWrapper} from "./gatherwrapper";
 
 global.WebSocket = require("isomorphic-ws");
 
@@ -11,6 +12,7 @@ const HELP_MESSAGE = "Enter /help for this list of commands:\n/rickroll <Name> t
 // gather game client setup
 const game = new Game(() => Promise.resolve({apiKey: GATHER_API_KEY}));
 game.connect(GATHER_SPACE_ID);
+const myWrapper = new GatherWrapper(game)
 
 function subscribeToMapSetObjects() {
     game.subscribeToEvent("mapSetObjects", (data, context) => {
@@ -50,55 +52,29 @@ function setJoinTrap(playerName: string, delay: number, callback: TrapCallback) 
 
 function setRickRollTrap(playerName: string) {
     setJoinTrap(playerName, 3000, (player, id) => {
-        const mewmba = new Mewmba(game);
-        mewmba.rickroll(id)
+        myWrapper.rickroll(id)
     })
 }
 // setRickRollTrap("4113")
 
-function randomRoomba() {
-    setTimeout(async () => {
-        const mewmba = new Mewmba(game)
-        mewmba.selectMewmba(mewmba.listMewmbas()[0]);
-        mewmba.routeToPoint(mewmba.getRandomPoint())
-    }, 3000)
-}
-
-// randomRoomba();
-
-function mewmbaMoveToPlayer(mewmbaName: string, playerName: string) {
-    game.subscribeToEvent("playerJoins", (data, context) => {
-        setTimeout(async () => {
-            const mewmba = new Mewmba(game);
-            mewmba.selectMewmba(mewmba.getMewmbaByName(mewmbaName));
-
-            mewmba.routeToPoint(mewmba.getPersonPoint(playerName));
-        }, 1000);
-    });
-}
-
-// mewmbaMoveToPlayer("phillis", "4113")
-
 function mewmbaHarassTheIntern(mewmbaName: string, playerName: string) {
     setJoinTrap(playerName, 1000, (player, id) => {
-        const mewmba = new Mewmba(game);
-        mewmba.selectMewmba(mewmba.getMewmbaByName(mewmbaName));
+        const mewmba = myWrapper.getMewmbaByName(mewmbaName);
         // mewmba.chasePlayer(playerName);
-        mewmba.createNeonLight(1, 1, "violet")
+        myWrapper.createNeonLight(1, 1, "violet")
     })
 }
 // mewmbaHarassTheIntern("4113", "Michael Black")
 
 function mewmbaSetUpDanceParty(mewmbaName: string, playerName: string) {
     setJoinTrap(playerName, 1000, (player, id) => {
-        const mewmba = new Mewmba(game);
-        mewmba.selectMewmba(mewmba.getMewmbaByName(mewmbaName));
+        const mewmba = myWrapper.getMewmbaByName(mewmbaName);
         // Range: (36,10) -> (45,20)
         mewmba.routeToPoint({x: 36, y: 10})
-        mewmba.createNeonLight(randomInt(36, 45), randomInt(10,20), RandomColor())
+        myWrapper.createNeonLight(randomInt(36, 45), randomInt(10,20), RandomColor())
     })
 }
-mewmbaSetUpDanceParty("phillis","phillis")
+// mewmbaSetUpDanceParty("phillis","phillis")
 
 game.subscribeToEvent("playerChats", (data, context) => {
     if (data.playerChats.contents.toLowerCase()[0] === "/") {
@@ -107,12 +83,11 @@ game.subscribeToEvent("playerChats", (data, context) => {
         switch (args[0]) {
             case "/rickroll":
                 if (args[1]) {
-                    const mewmba = new Mewmba(game);
 
                     for (const id in game.players) {
                         if (game.players[id].name.toLowerCase().includes(args[1].toLowerCase())) {
-                            mewmba.rickroll(id);
-                            // console.log(id, game.players[id]) 
+                            // console.log(id, game.players[id])
+                            myWrapper.rickroll(id);
                         }
                     }
                 }
